6d8697dd48edbf24f72602ca56af8ece994049b2
Santy <42287552+majorsanty@users.noreply.github.com>
2024-09-02 21:40:46 -0300

    v0.0.135

--- main/kubejs/server_scripts/recipes.js
+++ main/kubejs/server_scripts/recipes.js
@@ -1,117 +1,123 @@
-
-console.info('Hello, World! (Loaded server scripts)')
-ServerEvents.recipes(event => { //listen for the "recipes" server event.
+console.info("Hello, World! (Loaded server scripts)");
+ServerEvents.recipes((event) => {
+  //listen for the "recipes" server event.
   event.shaped(
-  Item.of('minecraft:chainmail_chestplate', 1), // arg 1: output
-  [
-    'A A',
-    'CBC', // arg 2: the shape (array of strings)
-    'ACA'
-  ],
-  {
-    A: 'minecraft:iron_nugget',
-    B: 'minecraft:leather_chestplate',
-    C: 'minecraft:chain'
-  }
-)
-event.shaped(
-  Item.of('minecraft:name_tag', 1), // arg 1: output
-  [
-    '  A',
-    ' C ', // arg 2: the shape (array of strings)
-    'B  '
-  ],
-  {
-    A: 'minecraft:paper',
-    B: 'minecraft:stick',
-    C: 'minecraft:chain'
-  }
-)
-event.shaped(
-  Item.of('minecraft:chainmail_leggings', 1), // arg 1: output
-  [
-    'AAA',
-    'CBC', // arg 2: the shape (array of strings)
-    'A A'
-  ],
-  {
-    A: 'minecraft:iron_nugget',
-    B: 'minecraft:leather_leggings',
-    C: 'minecraft:chain'
-  }
-)
-event.shaped(
-  Item.of('minecraft:chainmail_helmet', 1), // arg 1: output
-  [
-    'ACA',
-    'A A', // arg 2: the shape (array of strings)
-    '   '
-  ],
-  {
-    A: 'minecraft:iron_nugget',
-    C: 'minecraft:chain'
-  }
-)
-event.shaped(
-  Item.of('minecraft:chainmail_boots', 1), // arg 1: output
-  [
-    'C C',
-    'A A', // arg 2: the shape (array of strings)
-    '   '
-  ],
-  {
-    A: 'minecraft:iron_nugget',
-    C: 'minecraft:chain'
-  }
-)
+    Item.of("shadow-wizards:chain_chestplate", 1), // arg 1: output
+    [
+      "A A",
+      "CBC", // arg 2: the shape (array of strings)
+      "ACA",
+    ],
+    {
+      A: "minecraft:iron_nugget",
+      B: "shadow-wizards:gamberson_chestplate",
+      C: "minecraft:chain",
+    },
+  );
+  event.shaped(
+    Item.of("minecraft:name_tag", 1), // arg 1: output
+    [
+      "  A",
+      " C ", // arg 2: the shape (array of strings)
+      "B  ",
+    ],
+    {
+      A: "minecraft:paper",
+      B: "minecraft:stick",
+      C: "minecraft:chain",
+    },
+  );
+  event.shaped(
+    Item.of("shadow-wizards:chain_leggings", 1), // arg 1: output
+    [
+      "AAA",
+      "CBC", // arg 2: the shape (array of strings)
+      "A A",
+    ],
+    {
+      A: "minecraft:iron_nugget",
+      B: "shadow-wizards:gamberson_leggings",
+      C: "minecraft:chain",
+    },
+  );
+  event.shaped(
+    Item.of("shadow-wizards:chain_helmet", 1), // arg 1: output
+    [
+      "ACA",
+      "A A", // arg 2: the shape (array of strings)
+      "   ",
+    ],
+    {
+      A: "minecraft:iron_nugget",
+      C: "minecraft:chain",
+    },
+  );
+  event.shaped(
+    Item.of("shadow-wizards:chain_boots", 1), // arg 1: output
+    [
+      "C C",
+      "A A", // arg 2: the shape (array of strings)
+      "   ",
+    ],
+    {
+      A: "minecraft:iron_nugget",
+      C: "minecraft:chain",
+    },
+  );
 
-event.shaped(
-  Item.of('shadow-wizards:iron_rapier', 1), // arg 1: output
-  [
-    '  A',
-    'CA ', // arg 2: the shape (array of strings)
-    'BC '
-  ],
-  {
-    A: 'minecraft:iron_ingot',
-    B: 'minecraft:stick',
-    C: 'minecraft:chain'
-  }
-)
+  event.shaped(
+    Item.of("shadow-wizards:iron_rapier", 1), // arg 1: output
+    [
+      "  A",
+      "CA ", // arg 2: the shape (array of strings)
+      "BC ",
+    ],
+    {
+      A: "minecraft:iron_ingot",
+      B: "minecraft:stick",
+      C: "minecraft:chain",
+    },
+  );
 
-event.shapeless('4x shadow-wizards:fire_grease', [ // arg 1: output
-  'minecraft:bone_meal',
-  'minecraft:coal',
-  '4x shadow-wizards:resin'
-])
-event.shapeless('shadow-wizards:fire_grease', [ // arg 1: output
-  '2x minecraft:coal',
-  '2x shadow-wizards:resin'
-])
+  event.shapeless("4x shadow-wizards:fire_grease", [
+    // arg 1: output
+    "minecraft:bone_meal",
+    "minecraft:coal",
+    "4x shadow-wizards:resin",
+  ]);
+  event.shapeless("shadow-wizards:fire_grease", [
+    // arg 1: output
+    "2x minecraft:coal",
+    "2x shadow-wizards:resin",
+  ]);
 
-let minerals = ["ruby", "topaz", "citrine", "jade", "sapphire", "tanzanite"]
-minerals.forEach(element => {
-  event.replaceInput(
-    { input: 'minecraft:netherite_ingot' }, // Arg 1: the filter
-    'minecraft:netherite_ingot',            // Arg 2: the item to replace
-    'aylyth:esstline'         // Arg 3: the item to replace it with
-    // Note: tagged fluid ingredients do not work on Fabric, but tagged items do.
-  )
+  let minerals = ["ruby", "topaz", "citrine", "jade", "sapphire", "tanzanite"];
+  minerals.forEach((element) => {
+    event.replaceInput(
+      { input: "minecraft:netherite_ingot" }, // Arg 1: the filter
+      "minecraft:netherite_ingot", // Arg 2: the item to replace
+      "aylyth:esstline", // Arg 3: the item to replace it with
+      // Note: tagged fluid ingredients do not work on Fabric, but tagged items do.
+    );
+  });
+  event.remove({ output: "immersive_armors:wooden_helmet" });
+  event.remove({ output: "immersive_armors:wooden_chestplate" });
+  event.remove({ output: "immersive_armors:wooden_leggings" });
+  event.remove({ output: "immersive_armors:wooden_boots" });
 });
-event.remove({ output: 'immersive_armors:wooden_helmet' })
-event.remove({ output: 'immersive_armors:wooden_chestplate' })
-event.remove({ output: 'immersive_armors:wooden_leggings' })
-event.remove({ output: 'immersive_armors:wooden_boots' })
 
-})
-
-BlockEvents.rightClicked(event => {
-  if (event.block.id === "rootsclassic:mortar" && event.player.mainHandItem == "rootsclassic:pestle")
-  {
-    if(event.player.offHandItem == "minecraft:oak_sapling" || event.player.offHandItem == "minecraft:dark_oak_sapling") {
-      event.player.offHandItem.count--
-      event.player.give('shadow-wizards:resin');
-      event.cancel()
+BlockEvents.rightClicked((event) => {
+  if (
+    event.block.id === "rootsclassic:mortar" &&
+    event.player.mainHandItem == "rootsclassic:pestle"
+  ) {
+    if (
+      event.player.offHandItem == "minecraft:oak_sapling" ||
+      event.player.offHandItem == "minecraft:dark_oak_sapling"
+    ) {
+      event.player.offHandItem.count--;
+      event.player.give("shadow-wizards:resin");
+      event.cancel();
     }
   }
-})
+});
